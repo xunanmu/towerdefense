7ce6f45f6a11029e6532014d04e7f1e9ad97277a
徐卫平 <weiping.xu@appshahe.com>
2022-03-24 19:16:31 +0800

    feat(BaseCharacter.cpp): 初始BaseCharacter构造函数

--- Source/TowerDefense/Private/Character/BaseCharacter.cpp
+++ Source/TowerDefense/Private/Character/BaseCharacter.cpp
@@ -3,11 +3,40 @@
 
 #include "Character/BaseCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 // Sets default values
 ABaseCharacter::ABaseCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+
+	/*初始化人物的位置和朝向*/
+	GetMesh()->SetRelativeLocation(FVector(0,0,-90));
+	GetMesh()->SetRelativeRotation(FRotator(0,-90,0));
+	/*初始化人物摄像机手臂组件*/
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SPringArmComponent"));
+	/*设置摄像机手臂与人物之间的距离*/
+	SpringArmComponent->TargetArmLength = 500.0f;
+	/*设置根组件*/
+	SpringArmComponent->bUsePawnControlRotation = true;
+	SpringArmComponent->SetupAttachment(RootComponent);
+
+	/*初始摄像机*/
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	/*设置摄像机视野*/
+	CameraComponent->FieldOfView = 85.0f;
+	/*绑定到摄像机手臂组件*/
+	CameraComponent->SetupAttachment(SpringArmComponent);
+	/*初始化碰撞体*/
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
+	CapsuleComponent->SetCapsuleSize(34.0f,94.0f);
+	CapsuleComponent->SetupAttachment(RootComponent);
+	//初始速度
+	GetCharacterMovement()->MaxWalkSpeed = 700;
 
 }
 
