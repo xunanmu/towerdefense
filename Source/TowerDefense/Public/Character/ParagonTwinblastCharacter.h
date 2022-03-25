49ebb14e8473829be7e54b08a83f4b6c14319094
徐卫平 <weiping.xu@appshahe.com>
2022-03-25 10:28:12 +0800

    feat(ParagonTwinblastCharacter): 创建ParagonTwinblastCharacter

---
+++ Source/TowerDefense/Public/Character/ParagonTwinblastCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/BaseCharacter.h"
+#include "ParagonTwinblastCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_API AParagonTwinblastCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
