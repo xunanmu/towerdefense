d6f38a63b5f3e3c15e193e8e8f87253f62f0ab46
徐卫平 <weiping.xu@appshahe.com>
2022-03-25 11:49:55 +0800

    feat(CharacterPlayerController): 添加玩家控制类

---
+++ Source/TowerDefense/Private/PlayerController/CharacterPlayerController.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerController/CharacterPlayerController.h"
+
+ACharacterPlayerController::ACharacterPlayerController()
+{
+	/*默认显示鼠标*/
+	bShowMouseCursor = true;
+}
---
+++ Source/TowerDefense/Public/PlayerController/CharacterPlayerController.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CharacterPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_API ACharacterPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	ACharacterPlayerController();
+};
