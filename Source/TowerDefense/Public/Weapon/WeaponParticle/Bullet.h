7252c58bc909c49d81052c2d34b8313c4ad71363
徐卫平 <weiping.xu@appshahe.com>
2022-03-27 10:23:31 +0800

    feat(Bullet): 增加子弹类

---
+++ Source/TowerDefense/Public/Weapon/WeaponParticle/Bullet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon/WeaponParticle/BaseWeaponParticle.h"
+#include "Bullet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_API ABullet : public ABaseWeaponParticle
+{
+	GENERATED_BODY()
+	
+};
