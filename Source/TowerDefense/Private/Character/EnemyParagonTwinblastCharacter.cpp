6eeaa7aee66f7ac63a29676dc525364474df9d8d
徐卫平 <weiping.xu@appshahe.com>
2022-04-10 11:16:01 +0800

    优化代码结构，从角色类分出敌方角色

---
+++ Source/TowerDefense/Private/Character/EnemyParagonTwinblastCharacter.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/EnemyParagonTwinblastCharacter.h"
+
+AEnemyParagonTwinblastCharacter::AEnemyParagonTwinblastCharacter()
+{
+}
+
+void AEnemyParagonTwinblastCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
